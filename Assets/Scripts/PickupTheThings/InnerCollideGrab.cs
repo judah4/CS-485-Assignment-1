1be12ae689b471c73c8de163d316dbc3a828a661
Judah Perez <tythin4@gmail.com>
2017-09-08 10:14:09 -0700

    kata roller progress

---
+++ Assets/Scripts/PickupTheThings/InnerCollideGrab.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InnerCollideGrab : MonoBehaviour
+{
+    public PlayerControl PlayerControl;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other) 
+    {
+
+        Debug.Log("Collide" + other.gameObject.name);
+        
+        if (other.gameObject.CompareTag ("Pick Up"))
+        {
+            other.transform.parent = PlayerControl.transform;
+
+        }
+
+    }
+}
---
+++ Assets/Scripts/PickupTheThings/PlayerControl.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+	public float speed;
+    public float JumpPower = 10;
+
+    private Rigidbody rb;
+
+    public event Action<int> OnScore;
+
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+
+        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce (movement * (speed * rb.mass));
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * JumpPower*rb.mass, ForceMode.Impulse);
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            MassAndSizeIncrease();
+        }
+    }
+
+    void MassAndSizeIncrease()
+    {
+        rb.mass++;
+        transform.localScale = transform.localScale + Vector3.one;
+    }
+}
