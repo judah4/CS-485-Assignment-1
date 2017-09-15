50eef5416595dae8c57be49d30fc96cfe85a5ca2
Judah Perez <tythin4@gmail.com>
2017-09-15 14:03:52 -0700

    Camera and points for launching

--- Assets/Scripts/Launch/PlayerMove.cs
+++ Assets/Scripts/Launch/PlayerMove.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class PlayerMove : MonoBehaviour
 {
@@ -12,15 +13,25 @@ public class PlayerMove : MonoBehaviour
     public float JumpStrength = 10;
     public float MoveStrength = 10;
 
+    public Camera cam;
+    public MouseLook mouseLook = new MouseLook();
+
 	// Use this for initialization
 	void Start () {
-		
+		mouseLook.Init (transform, cam.transform);
+
 	}
-	
-	// Update is called once per frame
+
+    private void Update()
+    {
+        RotateView();
+    }
+
+    // Update is called once per frame
 	void FixedUpdate () {
+        
 		var input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        _rigidbody.AddForce(input * MoveStrength);
+        _rigidbody.AddForce(transform.TransformDirection(input) * MoveStrength);
 
 	    if (Input.GetKeyDown(KeyCode.Space) && jumpCount > 0)
 	    {
@@ -28,4 +39,21 @@ public class PlayerMove : MonoBehaviour
             _rigidbody.AddForce(Vector3.up * JumpStrength, ForceMode.Impulse);
 	    }
     }
+
+    private void RotateView()
+    {
+        //avoids the mouse looking if the game is effectively paused
+        if (Mathf.Abs(Time.timeScale) < float.Epsilon) return;
+
+        // get the rotation before it's changed
+        float oldYRotation = transform.eulerAngles.y;
+
+        mouseLook.LookRotation (transform, cam.transform);
+
+            // Rotate the rigidbody velocity to match the new direction that the character is looking
+            //Quaternion velRotation = Quaternion.AngleAxis(transform.eulerAngles.y - oldYRotation, Vector3.up);
+            //_rigidbody.velocity = velRotation*_rigidbody.velocity;
+            
+    }
+
 }
---
+++ Assets/Scripts/Launch/ScoreUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreUI : MonoBehaviour
+{
+    public PlayerMove Player;
+
+    public int Score = 0;
+    public Text ScoreText;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    Score = (int)Player.transform.position.z;
+	    ScoreText.text = "Distance: " + Score.ToString().PadLeft(6, '0');
+	}
+}
